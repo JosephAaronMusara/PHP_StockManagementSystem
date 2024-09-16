42a88bdfc6e2e33ce24d52ea89d61cddc7e9a8c7
JosephAaronMusara <josephaaronmusara@gmail.com>
2024-09-16 17:03:44 +0200

    Cookies and session & session security

--- dashboard/user_dashboard.php
+++ dashboard/user_dashboard.php
@@ -2,7 +2,7 @@
 session_start();
 if (!isset($_SESSION['user_id'])) {
     // Redirect to login page if not logged in
-    header("Location: login.php");
+    header("Location: ../accounts/login.php");
     exit;
 }
     ?>
