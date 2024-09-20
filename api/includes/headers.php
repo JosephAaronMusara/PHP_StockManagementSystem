b954bdc0fc8fea41bf14d903c7b11549191eed3b
JosephAaronMusara <josephaaronmusara@gmail.com>
2024-09-20 15:50:50 +0200

    added other http methods

--- api/includes/headers.php
+++ api/includes/headers.php
@@ -1,6 +1,5 @@
 <?php
-
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
-header("Access-Control-Allow-Methods: PUT");
+header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
 header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Access-Control-Allow-Methods, Content-Type, Authorization");
