fdbdc0596691ca57dfd06299cf737d26b83fec4d
JosephAaronMusara <josephaaronmusara@gmail.com>
2024-09-23 16:37:51 +0200

    Fully Functional

--- api/core/Category.php
+++ api/core/Category.php
@@ -1,5 +1,55 @@
 <?php
+class Category {
+    private $pdo;
 
-class Category{
+    public function __construct() {
+        $this->pdo = Database::getInstance()->getConnection();
+    }
 
+    public function getAllCategories() {
+        $stmt = $this->pdo->prepare("SELECT * FROM categories");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getCategoryById($id) {
+        $stmt = $this->pdo->prepare("SELECT * FROM categories WHERE id = ?");
+        $stmt->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function addCategory($data) {
+
+        $stmt = $this->pdo->prepare("SELECT * FROM categories WHERE name = ?");
+        $stmt->execute([$data['name']]);
+        if ($stmt->fetch()) {
+            return ['error' => 'Category already exists.'];
+        }
+        $stmt = $this->pdo->prepare("INSERT INTO categories (name) VALUES (?)");
+        if($stmt->execute([$data['name']])){
+            return ['id'=>$this->pdo->lastInsertId(),'message' => 'Category added successfully.'];
+        }
+    }
+
+    public function updateCategory($id, $data) {
+
+        $stmt = $this->pdo->prepare("SELECT * FROM categories WHERE name = ?  AND id != ?");
+        $stmt->execute([$data['name'],$id]);
+        if ($stmt->fetch()) {
+            return ['error' => 'Item already exists.'];
+        }
+        $stmt = $this->pdo->prepare("UPDATE categories SET name = ? WHERE id = ?");
+         if($stmt->execute([$data['name'], $id])){
+            return ['message' => 'Category updated successfully.'];
+        }
+        return ['error' => 'Failed to update Category.'];
+    }
+
+    public function deleteCategory($id) {
+        $stmt = $this->pdo->prepare("DELETE FROM categories WHERE id = ?");
+        if ($stmt->execute([$id])) {
+            return ['message' => 'Category deleted successfully.'];
+        }
+        return ['error' => 'Failed to delete Category.'];
+    }
 }
