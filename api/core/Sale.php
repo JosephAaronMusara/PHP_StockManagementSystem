fd3e7845dcb6e23669f45c3524029ade552aac42
JosephAaronMusara <josephaaronmusara@gmail.com>
2024-09-23 15:56:07 +0200

    Logic implemented

---
+++ api/core/Sale.php
@@ -0,0 +1,48 @@
+<?php
+class Sale
+{
+    private $pdo;
+
+    public function __construct()
+    {
+        $this->pdo = Database::getInstance()->getConnection();
+    }
+
+    public function getAllSales()
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM sales");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getSaleById($id)
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM sales WHERE id = ?");
+        $stmt->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function addSale($data)
+    {
+
+        $stmt = $this->pdo->prepare("INSERT INTO sales (user_id, total_amount) 
+                                    VALUES (?, ?)");
+        if ($stmt->execute([$data['total_amount'], $data['user_id']])) {
+            $stmt = $this->pdo->prepare("INSERT INTO sale_details (sale_id, stock_item_id, quantity, unit_price, user_id) 
+            VALUES (?, ?, ?, ?, ?)");
+            if ($stmt->execute([$this->pdo->lastInsertId(), $data['stock_item_id'], $data['quantity'], $data['unit_price'], $data['user_id']])) {
+                return ['id' => $this->pdo->lastInsertId(), 'message' => 'Sale added successfully.'];
+            }
+        }
+        return ['error' => 'Failed to record the sale.'];
+    }
+
+    public function deleteSale($id)
+    {
+        $stmt = $this->pdo->prepare("DELETE FROM sales WHERE id = ?");
+        if ($stmt->execute([$id])) {
+            return ['message' => 'Item deleted successfully.'];
+        }
+        return ['error' => 'Failed to delete item.'];
+    }
+}
