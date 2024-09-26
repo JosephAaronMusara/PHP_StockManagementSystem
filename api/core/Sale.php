41593ca7c6d28a9c4ef1aaeabc1b466ac86b35e1
JosephAaronMusara <josephaaronmusara@gmail.com>
2024-09-26 08:18:30 +0200

    Refactored query to return user readable data (JOINS)

--- api/core/Sale.php
+++ api/core/Sale.php
@@ -10,14 +10,24 @@ class Sale
 
     public function getAllSales()
     {
-        $stmt = $this->pdo->prepare("SELECT * FROM sales");
+        $query = "SELECT sales.id AS sale_id, sales.total_amount, sales.created_at AS sale_date, users.username AS sold_by, stock_items.name AS item_name, sale_details.quantity, sale_details.unit_price
+                    FROM stockmanagement.sales LEFT JOIN stockmanagement.sale_details ON sales.id = sale_details.sale_id
+                    LEFT JOIN stockmanagement.users ON sales.user_id = users.id LEFT JOIN stockmanagement.stock_items ON sale_details.stock_item_id = stock_items.id;";
+
+        $stmt = $this->pdo->prepare($query);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function getSaleById($id)
     {
-        $stmt = $this->pdo->prepare("SELECT * FROM sales WHERE id = ?");
+
+
+        $query = "SELECT sales.id AS sale_id, sales.total_amount, sales.created_at AS sale_date, users.username AS sold_by, stock_items.name AS item_name, sale_details.quantity, sale_details.unit_price
+        FROM stockmanagement.sales LEFT JOIN stockmanagement.sale_details ON sales.id = sale_details.sale_id
+        LEFT JOIN stockmanagement.users ON sales.user_id = users.id LEFT JOIN stockmanagement.stock_items ON sale_details.stock_item_id = stock_items.id WHERE sales.id=?";
+
+        $stmt = $this->pdo->prepare($query);
         $stmt->execute([$id]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
@@ -27,7 +37,7 @@ class Sale
 
         $stmt = $this->pdo->prepare("INSERT INTO sales (user_id, total_amount) 
                                     VALUES (?, ?)");
-        if ($stmt->execute([$data['user_id'],$data['total_amount']])) {
+        if ($stmt->execute([$data['user_id'], $data['total_amount']])) {
             $stmt = $this->pdo->prepare("INSERT INTO sale_details (sale_id, stock_item_id, quantity, unit_price) 
             VALUES (?, ?, ?, ?)");
             if ($stmt->execute([$this->pdo->lastInsertId(), $data['stock_item_id'], $data['quantity'], $data['unit_price']])) {
