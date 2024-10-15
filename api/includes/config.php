98e1a742d241fa75d769e198120d910143427bc4
JosephAaronMusara <josephaaronmusara@gmail.com>
2024-10-15 16:46:11 +0200

    increased cookie lifetime

--- api/includes/config.php
+++ api/includes/config.php
@@ -7,7 +7,7 @@ ini_set("session.use_only_cookies",1);
 ini_set("session.use_strict_mode",1);
 
 session_set_cookie_params([
-    'lifetime' => 1800,
+    'lifetime' => 18000,
     'domain' => 'localhost',
     'path' => '/',
     'secure' => true,
